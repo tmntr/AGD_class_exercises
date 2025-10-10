9c558825019c2bd3c13861e180fcc51c1abe5758
tmntr <tomnoy0809@highgateschool.org.uk>
2025-10-10 11:48:22 +0100

    Uploaded a fixed version of the shopping basket code to my REPOSITORY!

---
+++ OOP/Tom_Item.py
@@ -0,0 +1,7 @@
+class Item:
+    # Constructor
+    def __init__(self,name,description,price,stocklevel):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.stocklevel = stocklevel
