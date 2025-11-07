795aee421af90158076527d57527a44c27a4b74a
tmntr <tomnoy0809@highgateschool.org.uk>
2025-11-07 12:06:36 +0000

    We started some tkinter

---
+++ GUI/lets_try_some_tkinter.py
@@ -0,0 +1,31 @@
+import tkinter as tk
+
+class MainFrame(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.txt = tk.Label(self, text = 'Welcome to Tom\'s epic and most bodacious TKINTER THING!!!!')
+
+
+        self.btn = tk.Button(root, text = 'Do not press this button', fg = 'white', bg='red', activebackground= 'red4',activeforeground= 'white')
+        self.btn2 = tk.Button(root, text='Do not press this button either', fg='white', bg='blue', activebackground='blue4',
+                             activeforeground='white')
+        self.edt = tk.Entry(self)
+        self.sld = tk.Scale(self, from_=0, to=2000, orient=tk.VERTICAL)
+        self.config(bg='ivory2')
+        self.place_widgets()
+
+    def place_widgets(self):
+        self.txt.grid(row=0, column=0)
+        self.btn.grid(row=0, column=0)
+        self.btn2.grid(row=0, column=0)
+        self.edt.grid(row=1, column=1)
+        self.sld.grid(row=2, column=2)
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.title('Tom\'s epic and most bodacious TKINTER THING!!!!')
+    mainframe = MainFrame(root)
+    mainframe.grid()
+    root.mainloop()
+    #if btn.press
