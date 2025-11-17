05e0aaca74ae60eb27001030fd260ccc6aa6d1fa
tmntr <tomnoy0809@highgateschool.org.uk>
2025-11-17 14:06:47 +0000

    Added checkboxes and finished formatting the form

--- GUI/HW.py
+++ GUI/HW.py
@@ -12,38 +12,40 @@ class MainApp(tk.Tk):
 
         self.Form_frame.pack(side=tk.LEFT)
 
+
 class FormFrame(tk.Frame):
     def __init__(self, master):
         super().__init__(master)
 
-        self.txt = tk.Label(self, text = 'Please complete this form')
+        self.txt = tk.Label(self, text='Please complete this beautiful form',background='ivory3')
 
-        self.nametxt = tk.Label(self, text='Name:')
+        self.nametxt = tk.Label(self, text='Name:',background='ivory2')
         self.name = tk.Entry(self)
-        self.emailtxt = tk.Label(self, text='Email:')
+        self.emailtxt = tk.Label(self, text='Email:',background='ivory2')
         self.email = tk.Entry(self)
-        self.agetxt = tk.Label(self, text='Age:')
-        self.sld = tk.Scale(self, from_=0, to=100, orient=tk.HORIZONTAL)
-        self.submitbutton = tk.Button(self, text='Submit', foreground='black', background='ivory4',
-                                      activebackground='blue', activeforeground='black',width=2)
-        self.config(bg='ivory2')
+        self.agetxt = tk.Label(self, text='Age:',background='ivory2')
+        self.sld = tk.Scale(self, from_=0, to=100, length=200, orient=tk.HORIZONTAL,background='ivory2')
 
         self.genders = ['Male', 'Female', 'Other']
+
+        self.gendertxt = tk.Label(self, text='Gender:',background='ivory2')
 
         self.selected_gender = tk.StringVar()
         self.selected_gender.set(self.genders[0])
 
         self.radio_options = [tk.Radiobutton(self, text=gender,
-                                             value=gender, )
+                                             value=gender,background = 'ivory2')
                               for gender in self.genders]
+        self.codetxt = tk.Label(self, text='Coding languages:',background='ivory2')
+
+        self.checkbox_options = [tk.Checkbutton(self, text=item, onvalue=1, offvalue=0,background = 'ivory2') for item in
+                                 ["python", "javascript"]]
+
+        self.submitbutton = tk.Button(self, text='Submit', foreground='black', background='ivory4',
+                                      activebackground='blue', activeforeground='white', width=6)
+        self.config(bg='ivory2')
 
         self.place_widgets()
-
-
-
-        self.place_widgets()
-
-
 
     def place_widgets(self):
         self.txt.grid(row=0, column=0, columnspan=2)
@@ -53,11 +55,20 @@ class FormFrame(tk.Frame):
         self.email.grid(row=2, column=1)
         self.agetxt.grid(row=3, column=0)
         self.sld.grid(row=3, column=1)
-        self.submitbutton.grid(row=5, column=2,columnspan=2)
+        self.submitbutton.grid(row=6, column=1, columnspan=3)
+        self.gendertxt.grid(row=4, column=0)
+
         i = 1
         for ro in self.radio_options:
-            ro.grid(row = 4,column = i, padx=(5, 10), pady=5)
+            ro.grid(row=4, column=i, padx=(5, 10), pady=5)
             i += 1
+
+        self.codetxt.grid(row=5, column=0)
+
+        j = 1
+        for co in self.checkbox_options:
+            co.grid(row=5, column=j, padx=(5, 10), pady=5)
+            j += 1
 
 
 if __name__ == '__main__':
@@ -65,4 +76,6 @@ if __name__ == '__main__':
     mainframe.title('Tom\'s epic and most bodacious TKINTER THING!!!!')
     mainframe.grid()
     mainframe.mainloop()
-    #if btn.press
+
+
+# if btn.press
