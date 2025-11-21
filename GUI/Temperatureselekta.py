9ce8c3361d98b57278ce9d9d71dad76ce4869884
tmntr <tomnoy0809@highgateschool.org.uk>
2025-11-21 12:07:08 +0000

    Temperat improved

--- GUI/Temperatureselekta.py
+++ GUI/Temperatureselekta.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import temperature as temp
 
 class TempApp(tk.Tk):
     def __init__(self):
@@ -12,27 +13,36 @@ class TemperatureConvertingFrame(tk.Frame):
         super().__init__(master)
         self.master = master
 
-        self.og_temperature = tk.StringVar()
-        self.og_temperature_box = tk.Entry(self,textvariable = self.og_temperature)
+        self.og_temperature =temp.Temperature(0)
+
+        self.og_text_temp = tk.StringVar()
+
+        self.og_temperature_box = tk.Entry(self,textvariable=self.og_text_temp,width = 20)
 
         self.og_units = UnitsFrame(self)
 
         self.og_selected_unit = self.og_units.selected_unit
 
-        self.conv_temperature = tk.StringVar()
-        self.conv_temperature_box = tk.Entry(self, textvariable=self.og_temperature)
+        self.conv_temperature = temp.Temperature(0)
+
+        self.conv_text_temp = tk.StringVar()
+
+        self.conv_temperature_box = tk.Label(self, text=self.conv_text_temp.get(),background = 'white',width = 20)
 
         self.conv_units = UnitsFrame(self)
 
+        self.submit_button = tk.Button(self,background='cyan',foreground='blue',activebackground='blue',activeforeground='white',width = 20,text = 'CONVERT')
+
         self.place_widgets()
 
-    def place_widgets(self):
 
-        self.og_temperature_box.grid(row=0, column=0,sticky = 'w')
-        self.conv_temperature_box.grid(row=0, column=1, sticky='e')
-        self.submitbutton.grid(row=1)
-        self.og_units.grid(row=2, column=0, sticky='w')
-        self.conv_units.grid(row = 2, column = 1, sticky = 'e')
+    def place_widgets(self):
+        settings = {'padx':10,'pady':10}
+        self.og_temperature_box.grid(row=0, column=0,sticky = 'w',**settings)
+        self.conv_temperature_box.grid(row=0, column=1, sticky='e',**settings)
+        self.submit_button.grid(row=1,column=0,sticky='n',columnspan=2,**settings)
+        self.og_units.grid(row=2, column=0, sticky='w',**settings)
+        self.conv_units.grid(row = 2, column = 1, sticky = 'e',**settings)
 
 
     def calculate(self):
---
+++ GUI/temperature.py
@@ -0,0 +1,43 @@
+class Temperature:
+    def __init__(self, kelvin):
+        self._kelvin = kelvin
+
+    @property
+    def kelvin(self) -> float:
+        return self._kelvin
+
+    @kelvin.setter
+    def kelvin(self,kelvin:float):
+        self._kelvin = kelvin
+
+    @property
+    def celsius(self) -> float:
+        return self.kelvin - 273.15
+
+    @celsius.setter
+    def celsius(self,celsius:float):
+        if celsius < -273.15:
+            raise ValueError('This temperature is lower than absolute zero')
+        self._kelvin = celsius + 273.15
+
+    @property
+    def fahrenheit(self) -> float:
+        return self.celsius * 1.8 + 32
+
+    @fahrenheit.setter
+    def fahrenheit(self,fahrenheit:float):
+        if (fahrenheit-32)/1.8 < -273.15:
+            raise ValueError('This temperature is lower than absolute zero')
+        else:
+            self.celsius = (fahrenheit-32)/1.8
+
+    @property
+    def newton(self):
+        return (self.kelvin-273.15)/100*33
+
+    @newton.setter
+    def newton(self,newton:float):
+        if (newton*100/33)+273.15 < 0:
+            raise ValueError('This temperature is lower than absolute zero')
+        else:
+            self.kelvin = (newton*100/33)+273.15
