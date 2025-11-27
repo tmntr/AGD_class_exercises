a4cb1da59c744a4927a06458b46973fd52d5fb78
tmntr <tomnoy0809@highgateschool.org.uk>
2025-11-27 21:45:36 +0000

    Added cool background colour thing

--- GUI/Temperatureselekta.py
+++ GUI/Temperatureselekta.py
@@ -25,6 +25,10 @@ class TemperatureConvertingFrame(tk.Frame):
 
         self.conv_temperature = temp.Temperature(0)
 
+        self.temporarytemperature = tk.IntVar()
+
+        self.devslider = tk.Scale(self,variable =self.temporarytemperature,from_=0, to=1000, length=400, command=self.updatecolour)
+
         self.conv_text_temp = tk.StringVar()
 
         self.conv_temperature_box = tk.Label(self, text=self.conv_text_temp.get(),background = 'white',width = 20)
@@ -35,14 +39,40 @@ class TemperatureConvertingFrame(tk.Frame):
 
         self.place_widgets()
 
+    def updatecolour(self, temp):
+        self.config(background=self.get_current_colour(int(temp)))
 
     def place_widgets(self):
         settings = {'padx':10,'pady':10}
+
+
         self.og_temperature_box.grid(row=0, column=0,sticky = 'w',**settings)
         self.conv_temperature_box.grid(row=0, column=1, sticky='e',**settings)
         self.submit_button.grid(row=1,column=0,sticky='n',columnspan=2,**settings)
         self.og_units.grid(row=2, column=0, sticky='w',**settings)
         self.conv_units.grid(row = 2, column = 1, sticky = 'e',**settings)
+        self.devslider.grid(row = 3, column=0, sticky = 'w',**settings)
+
+    def from_rgb(self,rgb):
+        """translates an rgb tuple of int to a tkinter friendly color code
+        """
+        return "#%02x%02x%02x" % rgb
+
+
+    def get_current_colour(self,quantity):
+        if quantity < 273:
+            colour = (0,0,quantity/273*255)
+        elif quantity < 373:
+            colour = ((quantity-273)/100*255,0,255-(quantity-273)/100*255)
+        elif quantity < 1273:
+            colour = (255,(quantity-273)/1000*255,(quantity-273)/1000*255)
+        else:
+            colour = (255,255,255)
+
+        print(colour)
+        colourforuse = (int(colour[0]),int(colour[1]),int(colour[2]))
+        return self.from_rgb(colourforuse)
+
 
 
     def calculate(self):
