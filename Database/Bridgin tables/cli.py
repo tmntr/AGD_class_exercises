c76c165737889cbcca73dcd6c0e8c7a4e98f92bc
tmntr <tomnoy0809@highgateschool.org.uk>
2026-01-25 21:43:43 +0000

    You may now do all manner of things with clubs and people

--- Database/Bridgin tables/cli.py
+++ Database/Bridgin tables/cli.py
@@ -6,35 +6,101 @@ class CLI:
     def __init__(self):
         self.controller = Controller()
 
+    def show_activities(self):
+        activities = self.controller.get_activities()
+        for activity in activities:
+            print(activity)
+
     def show_person_activities(self):
         first_name = input('Enter first name: ')
         last_name = input('Enter last name: ')
         activities = self.controller.get_person_activities(first_name, last_name)
         for activity in activities:
             print(activity)
+
     def add_person_activities(self):
+        first_name = input('Enter the first name: ')
+        last_name = input('Enter the last name: ')
+        activity = input('Enter your desired activity: ')
+        #exists = self.controller.check_for_person(first_name, last_name)
+        #if exists:
+        #    print('Someone with that name already exists.')#, are you sure? (y/n)\n')
+            # if input().lower() == 'y':
+            #     self.controller.add_person(first_name, last_name)
+            # else:
+            #     print('Fair enough.')
+        #else:
+        self.controller.add_person_to_activity(first_name, last_name, activity)
+
+    def add_person(self):
+        first_name = input('Enter the first name: ')
+        last_name = input('Enter the last name: ')
+        self.controller.add_person(first_name, last_name)
+
+    def edit_name(self):
+        first_name = input('Enter the current first name: ')
+        last_name = input('Enter the current last name: ')
+        newfname = input('Enter the new first name: ')
+        newlname = input('Enter the new last name: ')
+        self.controller.edit_person(first_name, last_name, newfname, newlname)
+
+    def delete_person(self):
         first_name = input('Enter your first name: ')
         last_name = input('Enter your last name: ')
-        exists = self.controller.check_for_person(first_name, last_name)
-        if exists:
-            print('Someone with that name already exists, are you sure? (y/n)\n')
-            if input().lower() == 'y':
-                self.controller.add_person(first_name, last_name)
-            else:
-                print('Fair enough.')
-        else:
-            self.controller.add_person(first_name, last_name)
+        self.controller.delete_person(first_name, last_name)
+
+    def delete_person_activity(self):
+        first_name = input('Enter your first name: ')
+        last_name = input('Enter your last name: ')
+        activity = input('Enter your the activity name: ')
+        self.controller.delete_person_activity(first_name, last_name,activity)
+
+    def delete_activity(self):
+        activity = input('Enter your desired activity: ')
+        self.controller.delete_activity(activity)
+
+    def add_activity(self):
+        newactivity = input('Enter the name of the new activity: ')
+        self.controller.add_activity(newactivity)
+
+    def edit_activity(self):
+        currentname = input('Enter your desired activity: ')
+        newactivity = input('Enter the new name for your desired activity: ')
+        self.controller.edit_activity(currentname, newactivity)
+
+    def get_attendees(self):
+        activityname = input('Enter your desired activity: ')
+        attendees = self.controller.get_attendees(activityname)
+        for attendee in attendees:
+            print(attendee.first_name +' '+ attendee.last_name)
+
 
     def menu(self):
         print('Please choose from the following options:')
-        print('1. List available activities\n2. List a specific person\'s activities\n3.Add person')
+        print('1. List available activities\n2. List a specific person\'s activities\n3.Add person\n4. Edit person\n5. Delete person\n6. Enroll a person in an activity\n7. Remove someone from an activity\n8. Remove an activity\n9. Add an activity\n10. Edit an activity\n11. View an activity\'s attendees.')
         action = input()
         if action == '1':
-            self.show_person_activities()
+            self.show_activities()
         elif action == '2':
-            self.add_person_activities()
+            self.show_person_activities()
         elif action == '3':
+            self.add_person()
+        elif action == '4':
+            self.edit_name()
+        elif action == '5':
+            self.delete_person()
+        elif action == '6':
             self.add_person_activities()
+        elif action == '7':
+            self.delete_person_activity()
+        elif action == '8':
+            self.delete_activity()
+        elif action == '9':
+            self.add_activity()
+        elif action == '10':
+            self.edit_activity()
+        elif action == '11':
+            self.get_attendees()
         else:
             print('That was not an option. Please try again.')
 
@@ -42,4 +108,4 @@ if __name__ == '__main__':
     cli = CLI()
 
 while True:
-    cli.show_person_activities()
+    cli.menu()
--- Database/Bridgin tables/controller.py
+++ Database/Bridgin tables/controller.py
@@ -8,6 +8,11 @@ class Controller:
     def __init__(self, db_location = 'sqlite:///activities.sqlite'):
         self.engine = sa.create_engine(db_location)
 
+    def get_activities(self):
+        with so.Session(self.engine) as session:
+            activitynames = session.query(Activity.name).all()
+            return activitynames
+
     def get_person_activities(self, first_name, last_name):
         with so.Session(bind=self.engine) as session:
             stmt = sa.select(Person).where(Person.first_name == first_name and Person.last_name == last_name)
@@ -25,10 +30,32 @@ class Controller:
     def check_for_person(self,fname,lname):
         with so.Session(bind=self.engine) as session:
             stmt = sa.select(Person).where(Person.first_name == fname and Person.last_name == lname)
-            if len(stmt) > 0:
+            #print(stmt.execute())
+            user = [session.scalar(stmt)]
+            #print((session.scalar(stmt)))
+            if len(user) > 0:
                 return True
             else:
                 return False
+
+    def delete_person(self,fname,lname):
+        with so.Session(bind=self.engine) as session:
+            userget = sa.select(Person).where(Person.first_name == fname and Person.last_name == lname)
+            user = session.scalar(userget)
+            print(user)
+            session.delete(user)
+            session.commit()
+
+    def add_person_to_activity(self,fname,lname,activityname: str):
+        with so.Session(bind=self.engine) as session:
+            stmt = sa.select(Activity).where(Activity.name == activityname)
+            activity = session.scalar(stmt)
+            getuser = sa.select(Person).where(Person.first_name == fname and Person.last_name == lname)
+            user = session.scalar(getuser)
+            activity.attendees.append(user)
+            user.activities.append(activity)
+            session.commit()
+
 
 
     def add_person(self,fname, lname):
@@ -36,6 +63,59 @@ class Controller:
             new_person = Person(first_name=fname, last_name=lname)
             session.add(new_person)
             session.commit()
+
+    def change_name(self,fname,lname,nfname,nlname):
+        with so.Session(bind=self.engine) as session:
+            # nameedit = sa.update(Person).where(Person.first_name == fname and Person.last_name == lname).set(first_name=nfname, last_name=nlname)
+            # session.execute(nameedit)
+            personget = sa.select(Person).where(Person.first_name == fname and Person.last_name == lname)
+            person = session.scalar(personget)
+            person.first_name = nfname
+            person.last_name = nlname
+            session.commit()
+
+    def delete_person_activity(self,fname,lname,activityname: str):
+        with so.Session(bind=self.engine) as session:
+            stmt = sa.select(Activity).where(Activity.name == activityname)
+            activity = session.scalar(stmt)
+            userget = sa.select(Person).where(Person.first_name == fname and Person.last_name == lname)
+            user = session.scalar(userget)
+            activity.attendees.remove(user)
+            #user.activities.remove(activity)
+            session.commit()
+
+    def delete_activity(self,activityname: str):
+        with so.Session(bind=self.engine) as session:
+            activityget = sa.select(Activity).where(Activity.name == activityname)
+            activity = session.scalar(activityget)
+            session.delete(activity)
+            session.commit()
+
+    def edit_activity(self,currentname,newactivityname: str):
+        with so.Session(bind=self.engine) as session:
+            activityget = sa.select(Activity).where(Activity.name == newactivityname)
+            activity = session.scalar(activityget)
+            activity.name = newactivityname
+            session.commit()
+
+    def add_activity(self,activityname: str):
+        with so.Session(bind=self.engine) as session:
+            newactivity = Activity(name=activityname)
+            session.add(newactivity)
+            session.commit()
+
+
+    def get_attendees(self,activityname: str):
+        with so.Session(self.engine) as session:
+            activityget = sa.select(Activity).where(Activity.name == activityname)
+            activity = session.scalar(activityget)
+            return activity.attendees
+
+    # def delete_person(self,fname,lname):
+    #     with so.Session(bind=self.engine) as session:
+    #         pass
+    #         session.delete(Person)
+
 
 
 
